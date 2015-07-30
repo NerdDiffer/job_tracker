a39a9faa3cbd51f1ce7dfa840dd38488fd82509f
Rafael Espinoza <rafael@rafaelespinoza.com>
2015-07-29 18:23:25 -0700

    Add HomeController
    HomeController#index is the default route.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :contacts
 
   # default, root route
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
