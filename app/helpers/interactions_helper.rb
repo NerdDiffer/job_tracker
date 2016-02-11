5d496daba677a17832290f9adc9fefe414e58809
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-02-10 16:25:03 -0800

    Test InteractionsHelper

--- app/helpers/interactions_helper.rb
+++ app/helpers/interactions_helper.rb
@@ -1,14 +1,18 @@
 module InteractionsHelper
   def generate_select_opts_for_media
-    # To return a hash, you must group the 2nd parameter of this block
-    # with parentheses. Otherwise, it returns an array instead of a hash.
-    # The val is not needed at the moment.
-    # Writing the block parameters as `|new_media, key|` will return an array.
-    # This could also work, if you assign `key` as the value to
-    # `key[0].humanize`. But this is unintuitive to me.
-    Interaction.media.each_with_object({}) do |(key), new_media|
-      new_media[key.humanize] = key
+    # To return a hash, you must group the 1st block parameter with parens.
+    # Otherwise, it returns an array instead of a hash.
+    media = Interaction.media
+    media.each_with_object({}) do |(key), new_media|
+      humanized_key = humanize(key)
+      new_media[humanized_key] = key
       new_media
     end
+  end
+
+  private
+
+  def humanize(key)
+    key.humanize
   end
 end
--- spec/helpers/interactions_helper_spec.rb
+++ spec/helpers/interactions_helper_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe InteractionsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#generate_select_opts_for_media' do
+    media = { 'in_person' => 0, 'phone' => 1, 'email' => 2, 'other' => 3 }
+
+    before(:each) do
+      allow(Interaction).to receive(:media).and_return(media)
+    end
+
+    it 'calls humanize on each key' do
+      media_keys = media.keys
+      media_keys.each do |key|
+        expect(helper).to receive(:humanize).with(key)
+      end
+      helper.generate_select_opts_for_media
+    end
+  end
+
+  describe '#humanize' do
+    it 'calls #humanize on the input' do
+      input = 'foo'
+      expect(input).to receive(:humanize)
+      helper.send(:humanize, input)
+    end
+  end
 end
