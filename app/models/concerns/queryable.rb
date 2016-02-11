276498134265010229912ccde390455d6fd115c6
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-02-11 12:59:44 -0800

    Clean up Queryable.get_record_val_by

--- app/models/concerns/queryable.rb
+++ app/models/concerns/queryable.rb
@@ -28,36 +28,28 @@ module Queryable
 
     # Return a record's attribute by searching for some other attribute first.
     # Works with attributes on other models. Also works w/ virtual attributes.
-    # If searching by a virtual attribute, model must offer a corresponding
-    # class method in the naming pattern: '.find_by_*'.
-    # If that class method returns an ActiveRecord::Relation, then it may
-    # require further processing.
-    # Will return the first match only.
-    # TODO: Find a way to return several matches
+    #
+    # Caveats:
+    # - If searching by a virtual attribute, model must offer a corresponding
+    #   class method in the naming pattern: '.find_by_*'.
+    # - If that class method returns an ActiveRecord::Relation, then it may
+    #   require further processing.
+    # - Will return the first result only.
     # @param search_attr [String, Symbol], attribute to search by (virtual OK)
-    # @param value [String], value to search for within search_attribute
-    # @param options [Hash], set of named parameter options
+    # @param value [String], the attribute value to search for
+    # @param return_attr [String, Symbol], name of attribute to return
     # @return, the first matching record's id or nil
-    def get_record_val_by(attribute, value, options = {})
-      model       = options[:model] || self
-      return_attr = options[:return_attr] || :id
-      record = model.public_send("find_by_#{attribute}", value)
+    def get_record_val_by(attribute, value, return_attr = :id)
+      result = self.public_send("find_by_#{attribute}", value)
 
-      unless record.nil?
-        class_of_relation = check_relation_class(record)
-        class_of_relation.read_attribute(return_attr)
+      unless result.nil?
+        # TODO: Find a cleaner way to handle several matches
+        record = result.is_a?(ActiveRecord::Relation) ? result.first : result
+        record.read_attribute(return_attr)
       end
     end
 
     private
-
-    # If something descends from ActiveRecord::Relation, return first in list
-    # Otherwise, return the object
-    def check_relation_class(object_or_relation)
-      ancestors   = object_or_relation.class.ancestors
-      ar_included = ancestors.include?(ActiveRecord::Relation)
-      ar_included ? object_or_relation.first : object_or_relation
-    end
 
     def any_nil?(a, b)
       a.nil? || b.nil?
