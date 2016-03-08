26a8f7198dc7054fa13a2e44d9c731aa543f86f9
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-03-08 09:36:42 -0800

    Turn off action view logging for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Do not log any rendering of partials, layouts, etc
+  config.action_view.logger = nil
 end
