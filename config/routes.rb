cb79f5dcdc33e962d2b923cd695d7d9a6f1d996e
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-03-14 15:36:54 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   match '/search_suggestions', to: 'search_suggestions#index', via: :get
 
   # Sessions
-  get    'login'  => 'sessions#new'
-  post   'login'  => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  get '/auth/twitter/callback', to: 'sessions/provider_identities#create'
+  get '/auth/failure',          to: 'sessions/provider_identities#failure'
+  get    'login',  to: 'sessions/accounts#new'
+  post   'login',  to: 'sessions/accounts#create'
+  delete 'logout', to: 'sessions/base#destroy'
 
   # User accounts, profiles
-  resource :profile, controller: 'users', as: 'user'
+  get '/register', to: 'users/accounts#new'
+  resource :profile, controller: 'users', as: 'user', only: [:show, :destroy] do
+    resource :account, controller: 'users/accounts', except: :new
+  end
 
   resources :notes,         only: :index
   resources :cover_letters, only: :index, controller: 'job_applications/cover_letters'
@@ -24,6 +31,4 @@ Rails.application.routes.draw do
   resources :contacts do
     resources :notes, except: :index
   end
-
-  root 'home#index'
 end
