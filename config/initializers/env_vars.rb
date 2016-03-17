75eb88652d3dcf912ffaac426d650ab88a5bd598
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-03-17 12:38:13 -0700

    Refactor handling of env vars in config & remove EnvVars namespace

--- config/initializers/env_vars.rb
+++ config/initializers/env_vars.rb
@@ -1,14 +1,12 @@
-module EnvVars
-  class Application < Rails::Application
-    config.before_configuration do
-      env_file_name = Rails.root.join('config', 'env_vars.yml').to_s
+class Application < Rails::Application
+  config.before_configuration do
+    rails_env = Rails.env
+    env_file_name = Rails.root.join('config', 'env_vars.yml').to_s
 
-      if File.exist?(env_file_name)
-        env_file = YAML.load_file(env_file_name)
-        env = Rails.env
-        keys = env_file[env]
-        keys.each { |key, val| ENV[key.to_s] = val }
-      end
+    if rails_env != 'production' && File.exist?(env_file_name)
+      env_file = YAML.load_file(env_file_name)
+      keys = env_file[rails_env]
+      keys.each { |key, val| ENV[key.to_s] = val }
     end
   end
 end
