a61988e836adeb3b0acfbaaa4dc47da220d03ec6
Rafael Espinoza <rafael@rafaelespinoza.com>
2016-03-17 19:16:09 -0700

    Add FAQ to home & update HomeHelper
    Add an FAQ
    
    Remove a helper & add another to HomeHelper
    
    Call new FAQ partial, remove reference to removed helper method.
    Use new helper method from 'about'.
    
    Fix broken test for HomeHelper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,13 +1,5 @@
 module HomeHelper
-  def main_links
-    [
-      { name: 'Companies', path: companies_path },
-      { name: 'Categories', path: categories_path },
-      { name: 'Contacts', path: contacts_path },
-      { name: 'Notes', path: notes_path },
-      { name: 'Job Applications', path: job_applications_path },
-      { name: 'Postings', path: postings_path },
-      { name: 'Cover Letters', path: cover_letters_path }
-    ]
+  def target_blank
+    { target: '_blank' }
   end
 end
--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -1,20 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe HomeHelper, type: :helper do
-  describe '#main_links' do
-    it 'each key in the hash object is :name or :path' do
-      links = helper.main_links
-      links.each do |link_hash|
-        keys = link_hash.keys
-        expect(keys).to eq %I(name path)
-      end
-    end
-    it 'each value in the hash object is a String' do
-      links = helper.main_links
-      links.each do |link_hash|
-        vals = link_hash.values
-        expect(vals).to all be_a String
-      end
+describe HomeHelper, type: :helper do
+  describe '#target_blank' do
+    it 'returns this hash' do
+      expected = { target: '_blank' }
+      expect(helper.target_blank).to eq expected
     end
   end
 end
