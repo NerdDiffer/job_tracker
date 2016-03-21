377a553b7d9c41942c3cd041be63cb53efa27336
Tyler Willingham <tylerwillingham@gmail.com>
2016-03-21 17:51:57 -0500

    It's ok to be verbose in tests, in fact it's preferred
    In this particular example,  you're building up a before block for a
    single test. This isn't particularly ineffecient but it doesn't seem
    necessary.
    
    Additionally, there are things in your before block which are necessary
    for the test to eval correctly.
    
    On the mystery guest pattern:
    https://robots.thoughtbot.com/mystery-guest

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -5,17 +5,14 @@ RSpec.describe Company, type: :model do
 
   describe '.get_record_val_by' do
     context 'retrieving real attributes from associated models' do
-      before(:each) do
-        allow(described_class)
-          .to receive(:find_by_name)
-          .with(instance_of(String))
-          .and_return(company)
-      end
-
       it 'returns company name' do
         attribute = :name
         value = 'foo'
         return_attr = 'name'
+        allow(described_class)
+          .to receive(:find_by_name)
+          .with(instance_of(String))
+          .and_return(company)
 
         actual = described_class.get_record_val_by(attribute, value, return_attr)
 
